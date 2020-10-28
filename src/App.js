50c78b24e6da76357bf48363e7e36a89eba9f677
Jada Arnett <jada.arnett@gmail.com>
2020-10-28 19:24:21 -0400

    imported employee table into return

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import EmployeeTable from "./components/EmployeeTable"
 
 function App() {
   return (
     <div>
       <Header/>
+      <EmployeeTable/>
     </div>
   );
 }
