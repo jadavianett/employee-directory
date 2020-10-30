6f21911c2908a6db5533bd74069dbb156fcc2f8b
Jada Arnett <jada.arnett@gmail.com>
2020-10-30 09:57:23 -0400

    updated email row to change styling

--- src/components/EmployeeRow.jsx
+++ src/components/EmployeeRow.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-
 const EmployeeRow = (props) => {
     return (
+      
         <tbody>
         <tr>
           <th scope="row"><img alt={props.name} src={props.picture.thumbnail} /></th>
           <td>{props.name.first} {props.name.last}</td>
           <td>{props.phone}</td>
-          <td>{props.email}</td>
+          <td style={{width: "100px"}}>{props.email}</td>
         </tr>
       </tbody>
+  
     );
 };
 
