222a21073372b1be7cce3d93f1fae9f547060e8f
Jada Arnett <jada.arnett@gmail.com>
2020-10-30 09:58:02 -0400

    included employee row map on employee table component

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
+import EmployeeRow from "./EmployeeRow";
+import employees from "../employees.json";
 
 class EmployeeTable extends Component {
+  state = {
+    employees,
+  };
   render() {
     return (
-      <div>
-        <table class="table table-striped">
+      <div style={{margin: "0 auto"}}>
+        <table className="table table-striped">
           <thead>
             <tr>
               <th scope="col">Photo</th>
-              <th scope="col">Name</th>
+              <th scope="col"><span>Name</span></th>
               <th scope="col">Phone Number</th>
               <th scope="col">E-Mail</th>
             </tr>
           </thead>
+          {this.state.employees.map((employee) => (
+          <EmployeeRow name={employee.name} phone={employee.phone} email={employee.email} picture={employee.picture}/>
+        ))}
+        
         </table>
       </div>
     );
