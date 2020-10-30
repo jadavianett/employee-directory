6733b2a831df8704eb894438b35ab91406646bb1
Jada Arnett <jada.arnett@gmail.com>
2020-10-30 09:55:27 -0400

    switched to render just a header and employee table

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import Header from "./components/Header";
 import React, { Component } from "react";
 import EmployeeTable from "./components/EmployeeTable";
-import EmployeeRow from "./components/EmployeeRow";
-import employees from "./employees.json";
 
 class App extends Component {
-  state = {
-    employees,
-  };
+  
   render() {
     return (
       <div>
         <Header />
-        <EmployeeTable />
-        {this.state.employees.map((employee) => (
-          <EmployeeRow name={employee.name} phone={employee.phone} email={employee.email} picture={employee.picture}/>
-        ))}
+        <EmployeeTable/>
       </div>
     );
   }
