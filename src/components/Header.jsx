f878ed2a70b1bf9d4e51bb936b4ffa2ae51b078e
Jada Arnett <jada.arnett@gmail.com>
2020-10-31 16:05:49 -0400

    changing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div class="page-header">
   <h1>Employee Directory</h1>
   <br/>
-  <h5>Click on a table property to to filter or use the search bar to search for a specific employee.</h5>
+  <h5>Click on name property to sort employees alphabetically or use the search bar to search for a specific employee!</h5>
 </div>
   );
 };
