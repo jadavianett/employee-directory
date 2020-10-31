17b191d2cc8beb0100147941fe10018368d424f0
Jada Arnett <jada.arnett@gmail.com>
2020-10-31 14:59:38 -0400

    adding in contructor to manually bind this to a function

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -2,42 +2,50 @@ import React, { Component } from "react";
 import EmployeeRow from "./EmployeeRow";
 import employees from "../employees.json";
 
-
 class EmployeeTable extends Component {
   state = {
     employees,
-    names: []
+    names: [],
   };
 
-  componentDidMount() {
-    let {employees, names} = this.state;
-    names = this.state.employees.map(employee => employee.name);
-    this.setState({ names })
+  constructor(props) {
+    super(props);
+    this.sortAscending = this.sortAscending.bind(this);
   }
 
-sortAscending () {
-const { names } = this.state;
-names.sort((a, b) => a - b)    
-this.setState({ names })
-}
+  componentDidMount() {
+    let { names } = this.state.names;
+    names = this.state.employees.map((employee) => employee.name);
+    this.setState({ names });
+  }
 
+  sortAscending() {
+    const { names } = this.state;
+    names.sort((a, b) => a - b);
+  }
 
   render() {
     return (
-      <div style={{margin: "0 auto"}}>
+      <div style={{ margin: "0 auto" }}>
         <table className="table table-striped" id="employeeTable">
           <thead>
             <tr>
               <th scope="col">Photo</th>
-              <th scope="col" onClick = {this.sortAscending} ><span>Name</span></th>
+              <th scope="col" onClick={this.sortAscending}>
+                <span>Name</span>
+              </th>
               <th scope="col">Phone Number</th>
               <th scope="col">E-Mail</th>
             </tr>
           </thead>
           {this.state.employees.map((employee) => (
-          <EmployeeRow name={employee.name} phone={employee.phone} email={employee.email} picture={employee.picture}/>
-        ))}
-        
+            <EmployeeRow
+              name={employee.name}
+              phone={employee.phone}
+              email={employee.email}
+              picture={employee.picture}
+            />
+          ))}
         </table>
       </div>
     );
